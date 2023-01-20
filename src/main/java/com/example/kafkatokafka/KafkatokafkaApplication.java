3d332d6a6d8a531dde3f5858190655043d2e1561
Aishwarya Murali <Aishwarya@NTP-LAP-1173.chn.nestgroup.net>
2023-01-20 09:27:09 +0530

    first

---
+++ src/main/java/com/example/kafkatokafka/KafkatokafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkatokafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkatokafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkatokafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kafkatokafka/KafkatokafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkatokafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkatokafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
